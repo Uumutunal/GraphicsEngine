6c5d9ad7c5fad8eafe8389d332e775fb6a1b2b75
Uumutunal <unal.umut.98@gmail.com>
2024-07-22 17:59:30 +0300

    fill triangle

--- RenderEngine/RenderEngine.cpp
+++ RenderEngine/RenderEngine.cpp
@@ -1,7 +1,8 @@
 #include <SDL.h>
 #include<iostream>
 #include<vector>
-#include <list>
+
+#include"methods.h"
 
 using namespace std;
 
@@ -9,7 +10,6 @@ const int WIDTH = 640;
 const int HEIGHT = 640;
 SDL_Window* window = NULL;
 SDL_Renderer* renderer = NULL;
-
 
 
 float fNear = 300;
@@ -20,48 +20,24 @@ struct vec2d
 	float x, y;
 };
 
+
+
 struct vec3d
 {
 	float x, y, z;
 };
 
-
-
-struct triangle
-{
-	vec2d p[3];
-};
-
-struct triangle3d
-{
-	vec3d p[3];
-};
-
-
-
-struct mesh
-{
-	triangle tris[8];
-};
-
-struct mesh3d
-{
-
-	triangle3d tris[8];
-
-};
-
-//***********
-
 struct vertex {
 	vec3d p;
 	int id;
+	vec3d normal;
 };
-
-
 struct face {
 	vector<int> points;
 };
+
+
+
 
 struct meshv
 {
@@ -102,20 +78,6 @@ public:
 };
 
 
-void createMesh() {
-
-	float cube[3][2] = { {0.0f, 0.0f}, {0.0f, 1.0f}, {1.0f, 1.0f} };
-
-	triangle tri1 = { vec2d {0.0f, 0.0f}, vec2d {0.0f, 1.0f}, vec2d {1.0f, 1.0f} };
-
-	triangle mesh[] = { tri1 };
-
-
-}
-
-vector<int> a = { 5 };
-
-
 void drawTris(float tri[][2]) {
 
 
@@ -142,88 +104,7 @@ void drawTris(float tri[][2]) {
 }
 
 
-void drawTris(mesh m) {
 
-
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-
-
-	for (auto tri : m.tris) {
-
-		int j;
-		for (size_t i = 0; i < 3; i++)
-		{
-			if (i != 2) {
-				j = i + 1;
-			}
-			else {
-				j = 0;
-			}
-
-
-			SDL_Point points[2] = {
-			{tri.p[i].x + WIDTH / 2, tri.p[i].y + HEIGHT / 2},
-			{tri.p[j].x + WIDTH / 2, tri.p[j].y + HEIGHT / 2},
-			};
-
-			SDL_RenderDrawLines(renderer, points, 2);
-		}
-	}
-
-}
-
-void drawTris(mesh3d m) {
-
-
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-
-
-
-	for (auto tri : m.tris) {
-
-		int j;
-		for (size_t i = 0; i < 3; i++)
-		{
-			if (i != 2) {
-				j = i + 1;
-			}
-			else {
-				j = 0;
-			}
-
-
-			SDL_Point points[2] = {
-			{tri.p[i].x + WIDTH / 2, tri.p[i].y + HEIGHT / 2},
-			{tri.p[j].x + WIDTH / 2, tri.p[j].y + HEIGHT / 2},
-			};
-
-			SDL_RenderDrawLines(renderer, points, 2);
-		}
-	}
-
-}
-
-mesh3d& project(mesh3d& m) {
-
-	for (auto& tri : m.tris) {
-
-
-		for (size_t i = 0; i < 3; i++)
-		{
-
-			float z = tri.p[i].z;
-
-			float zRate = z / (fFar - fNear);
-
-			tri.p[i].x = tri.p[i].x * zRate;
-			tri.p[i].y = tri.p[i].y * zRate;
-
-		}
-	}
-
-	return m;
-}
 
 meshv& project(meshv& m) {
 
@@ -240,6 +121,154 @@ meshv& project(meshv& m) {
 	}
 
 	return m;
+}
+
+
+vec3d subVector(vec3d vec1, vec3d vec2) {
+	vec3d subbed;
+	subbed.x = vec1.x - vec2.x;
+	subbed.y = vec1.y - vec2.y;
+	subbed.z = vec1.z - vec2.z;
+
+	return subbed;
+}
+
+bool fillTriangle2(vec3d p, vec3d a, vec3d b, vec3d c) {
+	// Compute vectors
+	vec2d v0 = { c.x - a.x, c.y - a.y };
+	vec2d v1 = { b.x - a.x, b.y - a.y };
+	vec2d v2 = { p.x - a.x, p.y - a.y };
+
+	// Compute dot products
+	float dot00 = v0.x * v0.x + v0.y * v0.y;
+	float dot01 = v0.x * v1.x + v0.y * v1.y;
+	float dot02 = v0.x * v2.x + v0.y * v2.y;
+	float dot11 = v1.x * v1.x + v1.y * v1.y;
+	float dot12 = v1.x * v2.x + v1.y * v2.y;
+
+	// Compute barycentric coordinates
+	float invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
+	float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
+	float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
+
+	// Check if point is in triangle
+	return (u >= 0) && (v >= 0) && (u + v <= 1);
+}
+
+
+void fillTriangle(vector<vec3d> vertices) {
+
+	if (vertices.size() < 3) {
+		return;
+	}
+	vertices.push_back(vertices[0]);
+	vertices.push_back(vertices[1]);
+
+
+	vec3d longestEdge[2];
+	vec3d thirdVertex;
+	float edgeLengthSquared = -1;
+
+	for (size_t i = 0; i < vertices.size() - 2; i++)
+	{
+		vec3d edge = subVector(vertices[i + 1], vertices[i]);
+
+		float temp = edge.x * edge.x + edge.y * edge.y;
+		if (edgeLengthSquared < temp) {
+			edgeLengthSquared = temp;
+
+			longestEdge[0] = vertices[i];
+			longestEdge[1] = vertices[i + 1];
+			thirdVertex = vertices[i + 2];
+
+		}
+
+	}
+
+
+	//Equation for the longest edge, Ax + By + C = 0
+	//A = y2 - y1
+	//B = x1 - x2
+	//C = (x2 * y1) - (x1 * y2)
+	float a = longestEdge[1].y - longestEdge[0].y;
+	float b = longestEdge[0].x - longestEdge[1].x;
+	float c = (longestEdge[1].x * longestEdge[0].y) - (longestEdge[0].x * longestEdge[1].y);
+
+	//Equation for the parallel line that passes through the third point, Ax + By + D = 0
+	float d = -a * thirdVertex.x - b * thirdVertex.y;
+
+
+	//Equations for the other edges
+	float a2 = longestEdge[1].y - thirdVertex.y;
+	float b2 = thirdVertex.x - longestEdge[1].x;
+	float c2 = (longestEdge[1].x * thirdVertex.y) - (thirdVertex.x * longestEdge[1].y);
+
+	float a3 = thirdVertex.y - longestEdge[0].y;
+	float b3 = longestEdge[0].x - thirdVertex.x;
+	float c3 = (thirdVertex.x * longestEdge[0].y) - (longestEdge[0].x * thirdVertex.y);
+
+	//Loop through the points on the longest edge
+
+
+	for (float x = longestEdge[0].x; x < longestEdge[1].x; x++)
+	{
+		//Calculate the y for x
+		float y = -(a * x + c) / b;
+
+		//Calculate perpendicular line
+		float aP = -b;
+		float bP = a;
+
+		float cP = -(aP * x + bP * y);
+
+
+		//Find intersection point
+		float determinant = a * bP - aP * b;
+
+		float x2 = (b * cP - bP * d) / determinant;
+		float y2 = (aP * d - a * cP) / determinant;
+
+		int sign2 = (a2 * longestEdge[0].x + b2 * longestEdge[0].y + c2);
+
+		int sign3 = (a3 * longestEdge[1].x + b3 * longestEdge[1].y + c3);
+
+		int s2;
+		int s3;
+
+		if (sign2 < 0) {
+			s2 = 1;
+		}
+		else {
+			s2 = -1;
+		}
+
+		if (sign3 < 0) {
+			s3 = 1;
+		}
+		else {
+			s3 = -1;
+		}
+
+		for (float i = x; i < x2; i++)	
+		{
+			vec3d p;
+			p.x = i;
+			p.y = (aP * x + cP) / -bP;
+
+			//Fix <> signs
+			if ((a2 * p.x + b2 * p.y + c2)*s2 < 0 && (a3 * p.x + b3 * p.y + c3)*s3 < 0) {
+
+				SDL_RenderDrawPoint(renderer, p.x + 320, p.y + 320);
+
+			}
+			else {
+				break;
+			}
+		}
+
+	}
+
+
 }
 
 void drawTris(meshv m) {
@@ -274,6 +303,8 @@ void drawTris(meshv m) {
 
 		}
 
+		fillTriangle(pos);
+
 		for (size_t i = 0; i < 3; i++) {
 			if (i != 2) {
 				j = i + 1;
@@ -291,37 +322,10 @@ void drawTris(meshv m) {
 			SDL_RenderDrawLines(renderer, points, 2);
 		}
 
-
 	}
 
-
 }
 
-
-
-
-
-bool fillTriangle(vec3d p, vec3d a, vec3d b, vec3d c) {
-	// Compute vectors
-	vec2d v0 = { c.x - a.x, c.y - a.y };
-	vec2d v1 = { b.x - a.x, b.y - a.y };
-	vec2d v2 = { p.x - a.x, p.y - a.y };
-
-	// Compute dot products
-	float dot00 = v0.x * v0.x + v0.y * v0.y;
-	float dot01 = v0.x * v1.x + v0.y * v1.y;
-	float dot02 = v0.x * v2.x + v0.y * v2.y;
-	float dot11 = v1.x * v1.x + v1.y * v1.y;
-	float dot12 = v1.x * v2.x + v1.y * v2.y;
-
-	// Compute barycentric coordinates
-	float invDenom = 1 / (dot00 * dot11 - dot01 * dot01);
-	float u = (dot11 * dot02 - dot01 * dot12) * invDenom;
-	float v = (dot00 * dot12 - dot01 * dot02) * invDenom;
-
-	// Check if point is in triangle
-	return (u >= 0) && (v >= 0) && (u + v <= 1);
-}
 
 // Cross product
 vec3d cross(const vec3d& a, const vec3d& b) {
@@ -351,13 +355,15 @@ vec3d addVector(vec3d vec1, vec3d vec2) {
 	return added;
 }
 
+
+
 // Rotate point around an axis
 vec3d rotateAroundPoint(const vec3d& point, const vec3d mCenter, const vec3d& axis, float angle) {
 	// Translate point to origin
 	vec3d center;
 	center.x = mCenter.x;
 	center.y = mCenter.y;
-	center.z = mCenter.z - 450;
+	center.z = 50;
 
 	vec3d p = { point.x - center.x, point.y - center.y, point.z - center.z };
 
@@ -383,7 +389,6 @@ void move(meshv& m, vec3d v) {
 
 void rotate(meshv& m, const vec3d& axis, float angle) {
 
-
 	for (auto& vert : m.vertices) {
 
 		vec3d rotated = rotateAroundPoint(vert.p, m.position, axis, angle);
@@ -397,11 +402,11 @@ void rotate(meshv& m, const vec3d& axis, float angle) {
 
 
 
+
+
+
 int main(int argc, char* argv[])
 {
-
-
-
 
 	SDL_Init(SDL_INIT_VIDEO);
 	window = SDL_CreateWindow("Graphics Engine", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, WIDTH, HEIGHT, SDL_WINDOW_SHOWN);
@@ -409,10 +414,7 @@ int main(int argc, char* argv[])
 
 
 
-	createMesh();
-
 	int close = 0;
-
 
 
 
@@ -420,41 +422,7 @@ int main(int argc, char* argv[])
 	SDL_RenderClear(renderer);
 
 
-	//SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-	//SDL_RenderClear(renderer);
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
-	//for (int i = 0; i < 640; ++i)
-	//	SDL_RenderDrawPoint(renderer, i, i);
-
-
-
-	SDL_Point points[2] = {
-	{0, 0},
-	{640, 640},
-	};
-
-	//SDL_RenderDrawLines(renderer, points, 2);
-
-	SDL_Point points2[2] = {
-	{0, 640},
-	{640, 0},
-	};
-
-	//SDL_RenderDrawLines(renderer, points2, 2);
-
-
-	//float tri[3][2] = { {50.0f, 50.0f}, {100.0f, 50.0f}, {50.0f, 100.0f} };
-	triangle tri = { vec2d {50.0f, 50.0f}, {100.0f, 50.0f}, {50.0f, 100.0f} };
-
-	mesh cube;
-	cube.tris[0] = { vec2d {50.0f, 50.0f}, {100.0f, 50.0f}, {50.0f, 100.0f} };
-	cube.tris[1] = { vec2d {100.0f, 50.0f}, {100.0f, 100.0f}, {50.0f, 100.0f} };
-
-	cube.tris[2] = { vec2d {50.0f, 50.0f}, {100.0f, 50.0f}, {50.0f, 100.0f} };
-	cube.tris[3] = { vec2d {100.0f, 50.0f}, {100.0f, 100.0f}, {50.0f, 100.0f} };
-
-
-
 
 
 	SDL_RenderPresent(renderer);
@@ -483,10 +451,14 @@ int main(int argc, char* argv[])
 		{-50.0f, 50.0f, z2}, {50.0f, 50.0f, z2}, {-50.0f, -50.0f, z2},{50.0f, -50.0f, z2},
 		}
 	);
-	cubeV.addFace({ {0,1,2},{1,3,2},{4,5,6},
-		{5,7,6},{4,0,6},{6,0,2},
-		{4,5,0},{5,1,0},{1,5,3},
-		{3,5,7},{2,3,6},{3,7,6} });
+
+	//cubeV.addFace({ {0,1,2},{1,3,2},{4,5,6},
+	//	{5,7,6},{4,0,6},{6,0,2},
+	//	{4,5,0},{5,1,0},{1,5,3},
+	//	{3,5,7},{2,3,6},{3,7,6} });
+
+
+	cubeV.addFace({ {0,1,2} });
 
 	//move(cubeV, { 100,100,25 });
 
@@ -507,7 +479,7 @@ int main(int argc, char* argv[])
 		SDL_Event event;
 
 
-		rotate(cubeV, { 1, 1, 1 }, 3.14159f / 250);
+		//rotate(cubeV, { 1, 1, 1 }, 3.14159f / 250);
 
 		drawTris(cubeV);
 
@@ -525,20 +497,20 @@ int main(int argc, char* argv[])
 				switch (event.key.keysym.scancode) {
 				case SDL_SCANCODE_W:
 				case SDL_SCANCODE_UP:
-					move(cubeV, { 0,-10,0 });
+					move(cubeV, { 0,10,0 });
 					//dest.y -= speed / 30;
 					break;
 				case SDL_SCANCODE_A:
 				case SDL_SCANCODE_LEFT:
-					move(cubeV, { -10,0,0 });
+					move(cubeV, { 10,0,0 });
 					break;
 				case SDL_SCANCODE_S:
 				case SDL_SCANCODE_DOWN:
-					move(cubeV, { 0,10,0 });
+					move(cubeV, { 0,-10,0 });
 					break;
 				case SDL_SCANCODE_D:
 				case SDL_SCANCODE_RIGHT:
-					move(cubeV, { 10,0,0 });
+					move(cubeV, { -10,0,0 });
 					break;
 				case SDL_SCANCODE_G:
 
@@ -551,21 +523,6 @@ int main(int argc, char* argv[])
 		}
 
 
-
-
-
-
-		mesh3d cube3d;
-		cube3d.tris[0] = { vec3d {50.0f, 50.0f, z1}, {100.0f, 50.0f, z1}, {50.0f, 100.0f, z1} };
-		cube3d.tris[1] = { vec3d {100.0f, 50.0f, z1}, {100.0f, 100.0f, z1}, {50.0f, 100.0f, z1} };
-
-		cube3d.tris[2] = { vec3d {50.0f, 50.0f, z2}, {100.0f, 50.0f, z2}, {50.0f, 100.0f, z2} };
-		cube3d.tris[3] = { vec3d {100.0f, 50.0f, z2}, {100.0f, 100.0f, z2}, {50.0f, 100.0f, z2} };
-
-		//project(cube3d);
-
-		//drawTris(cube);
-		//drawTris(cube3d);
 
 
 
---
+++ RenderEngine/methods.h
@@ -0,0 +1,9 @@
+#pragma once
+
+#ifndef METHODS_H
+#define METHODS_H
+
+
+
+
+#endif
